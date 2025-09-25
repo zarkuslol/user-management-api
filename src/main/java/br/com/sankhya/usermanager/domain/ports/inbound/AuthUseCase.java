9e7215250e130c8b4df5d6ccf34fdab6449ab6c3
zarkuslol <lucasferlima8@gmail.com>
2025-09-25 02:24:06 -0300

    chore(auth): removing request password reset and password reseting (not necessary)

--- src/main/java/br/com/sankhya/usermanager/domain/ports/inbound/AuthUseCase.java
+++ src/main/java/br/com/sankhya/usermanager/domain/ports/inbound/AuthUseCase.java
@@ -12,16 +12,4 @@ public interface AuthUseCase {
      * @return A response object containing the JWT.
      */
     LoginResponse login(LoginCommand command);
-
-    /**
-     * Inicia o processo de redefinição de senha para um usuário.
-     * @param command Contém o e-mail do usuário.
-     */
-    void requestPasswordReset(ForgotPasswordCommand command);
-
-    /**
-     * Efetiva a redefinição de senha usando um token válido.
-     * @param command Contém o token e a nova senha.
-     */
-    void resetPassword(ResetPasswordCommand command);
 }
